816a37ad910337b7c14d04cc1a04e9cd944ad55b
Ekarat Rattagan <pokekarat@gmail.com>
2014-06-19 14:35:00 +0800

    v1
    This is real

---
+++ src/com/example/trainandroid/Config.java
@@ -0,0 +1,7 @@
+package com.example.trainandroid;
+
+public class Config {
+
+	public static int sample = 0;
+	public static boolean processing;
+}
---
+++ src/com/example/trainandroid/Util.java
@@ -0,0 +1,21 @@
+package com.example.trainandroid;
+
+import android.os.SystemClock;
+
+public class Util {
+
+	public static boolean isInteger(String text) {
+		  try {
+			  
+		    new Integer(text);
+		    return true;
+		  } catch (NumberFormatException e) {
+		    return false;
+		  }
+	}
+	
+	public static void delay(int delayTime){
+		
+		SystemClock.sleep(delayTime);
+	}
+}
